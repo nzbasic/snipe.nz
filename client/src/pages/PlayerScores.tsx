c2c337d1fd994f6912a7cd872d8e80e1f1e59124
nzbasic <jcop589@aucklanduni.ac.nz>
2021-09-03 16:24:17 +1200

    show score on table

--- client/src/pages/PlayerScores.tsx
+++ client/src/pages/PlayerScores.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps } from 'react-router-dom'
 import { Play } from '../../../models/play'
 import axios from 'axios'
 import { Player } from '../../../models/Player.model';
+import NumberFormat from 'react-number-format'
 
 export const PlayerScores = (props: RouteComponentProps<{ id: string }>) => {
     const [plays, setPlays] = useState<Play[]>([])
@@ -34,12 +35,14 @@ export const PlayerScores = (props: RouteComponentProps<{ id: string }>) => {
         <div className="flex space-x-4 my-4">
             <a href={"https://osu.ppy.sh/users/" + player.id} target="_blank" rel="noreferrer" className="text-blue-400 hover:underline">{player.name}</a>
         </div>
+
         {!isLoading ? plays.map((play, index) => (
             <div key={play.id} className="flex text-xs lg:text-base space-x-2">
                 <span className="w-8">{(index+1) + ((pageNumber-1) * pageSize)}</span>
                 <span className="w-16 lg:w-28 truncate">{play.artist}</span>
                 <a href={"https://osu.ppy.sh/beatmaps/" + play.beatmapId} target="_blank" rel="noreferrer" className="truncate w-32 lg:w-60 text-blue-400 hover:underline">{play.title}</a>
                 <span className="w-20 lg:w-40 truncate">[{play.difficulty}]</span>
+                <NumberFormat className="w-24" value={play.score} displayType={'text'} thousandSeparator={true}/>
                 <span className="w-12 lg:w-16">{(play.pp??0).toFixed(0)}pp</span>
                 <span className="w-16 hidden lg:block">{(play.acc*100).toFixed(2)}%</span>
                 <span className="w-20 hidden lg:block truncate">{play.mods.join("")}</span>

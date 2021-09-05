9c7cb1dfac7e38df386ec52579d408c841966fbb
nzbasic <jcop589@aucklanduni.ac.nz>
2021-09-06 11:25:24 +1200

    change styling

--- client/src/components/SingleActivity.tsx
+++ client/src/components/SingleActivity.tsx
@@ -2,13 +2,13 @@ import { FormattedSnipe } from "../../../models/Snipe.model";
 
 export const SingleActivity = ({snipe}: { snipe: FormattedSnipe }) => {
     return (
-        <div className="flex flex-row text-xs lg:text-base lg:max-w-7xl items-center space-x-1">
+        <div className="flex flex-row text-xs lg:text-base lg:max-w-7xl max-w-xs sm:max-w-md items-center space-x-1">
             <span className="hidden md:block truncate">{new Date(snipe.time).toLocaleString()}:</span>
-            <a href={"/player/" + snipe.sniperId} className="hover:underline truncate">{snipe.sniper}</a>
+            <a href={"/player/" + snipe.sniperId} className="hover:underline truncate text-green-400">{snipe.sniper}</a>
             <span>sniped</span>
-            <a href={"/player/" + snipe.victimId} className="hover:underline truncate">{snipe.victim}</a>
+            <a href={"/player/" + snipe.victimId} className="hover:underline truncate text-red-400">{snipe.victim}</a>
             <span>on</span>
-            <a href={"https://osu.ppy.sh/beatmaps/" + snipe.beatmapId} target="_blank" rel="noreferrer" className="hover:underline truncate">{snipe.beatmap}</a>
+            <a href={"https://osu.ppy.sh/beatmaps/" + snipe.beatmapId} target="_blank" rel="noreferrer" className="hover:underline truncate text-blue-400">{snipe.beatmap}</a>
         </div>
     )
 }

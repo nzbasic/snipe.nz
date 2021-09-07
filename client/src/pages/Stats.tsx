bf4355f50d0de9afa32d23148eb90181ec305aac
nzbasic <jcop589@aucklanduni.ac.nz>
2021-09-08 11:10:13 +1200

    add stats page

--- client/src/pages/Stats.tsx
+++ client/src/pages/Stats.tsx
@@ -2,15 +2,8 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 export const Stats = () => {
-
-    useEffect(() => {
-        axios.get("/api/activity/topSniped", { params: { isSniper: true, pageNumber: 1, pageSize: 10 }}).then(res => {
-            console.log(res.data)
-        })
-    }, [])
-
     return (
-        <div>
+        <div className="flex flex-col">
 
         </div>
     )

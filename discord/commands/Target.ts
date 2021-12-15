072562cddc439321d089dcdf9ebd7828fe975931
nzbasic <jcop589@aucklanduni.ac.nz>
2021-12-15 14:24:46 +1300

    add command for current snipe target

---
+++ discord/commands/Target.ts
@@ -0,0 +1,16 @@
+import {
+    Alias,
+    Command,
+    Client
+} from "@frasermcc/overcord";
+import axios from "axios";
+import { Message } from "discord.js";
+
+@Alias("!snipe-target")
+export default class Target extends Command {
+    async execute(message: Message, client: Client) {
+        const res = await axios.get("https://snipe.nz/api/target")
+        const target = res.data.target
+        message.channel.send(`Current Target: ${target.player.name}. Ends <t:${target.dateEnd / 1000}:R>`)
+    }
+}

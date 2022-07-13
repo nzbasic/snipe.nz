3f4e943cc52c27411586839059c10d30ccb616c6
James <54062686+nzbasic@users.noreply.github.com>
2022-07-13 18:44:27 +1200

    update collect script

--- collection/collect.ts
+++ collection/collect.ts
@@ -8,35 +8,29 @@ import { getCookieJar } from '../shared/jar';
 import fg from 'fast-glob';
 import { PlayerModel } from '../models/Player.model';
 import { convertToObject } from 'typescript';
+import { Database as Sqlite3, Statement } from "sqlite3";
+import { open, Database } from "sqlite";
+import { IdModel } from '../models/Id.model'
+
 dotenv.config();
 
 (async () => {
-    console.log("connecting")
     const mongo = process.env.MONGO??""
-    await mongoose.connect(mongo);
-   
-    const data = await fs.promises.readFile("ids.json")
-    const ids: number[] = JSON.parse(data.toString())
+    await mongoose.connect(mongo)
+
+    const ids = (await IdModel.find()).map(x => x.id as number)
     const jar = await getCookieJar()
 
-    let i = 0;
-    for (const id of ids.slice(7000)) {
-        if (i % 10 == 0) {
+    for (let i = 0; i < ids.length; i++) {
+        if (i % 100 == 0) {
             console.log(i + " / " + ids.length)
         }
 
-        let prev = new Date().getTime()
         try { 
-            await updateBeatmap(id, jar, prev)
+            await updateBeatmap(ids[i], jar)
         } catch(err) {
-            const difference = 5000 - (new Date().getTime() - prev)
-            // ensure no more than 2 requests per second
-            if (difference > 0) {
-                await new Promise(resolve => setTimeout(resolve, difference))
-            }
             console.log(err)
         }
-        i++
     }
 })()
 

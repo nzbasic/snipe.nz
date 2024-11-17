1360c34112afbf82332b12e5e24228f48c50fd4a
James <54062686+nzbasic@users.noreply.github.com>
2024-11-17 14:44:24 +1300

    Remove ALL select

--- app/Livewire/Leaderboard.php
+++ app/Livewire/Leaderboard.php
@@ -49,6 +49,7 @@ class Leaderboard extends Component implements HasForms, HasTable
         return $table
             ->query(LeaderboardView::query())
             ->defaultSort('total_firsts', 'desc')
+            ->paginationPageOptions([10, 25, 50])
             ->recordUrl(
                 fn (LeaderboardView $record): string => route('players.show', ['player' => $record->user_id]),
             )

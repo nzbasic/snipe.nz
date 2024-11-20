9a9c8577f79d690fbf808c14df2fa864c525062b
James <54062686+nzbasic@users.noreply.github.com>
2024-11-20 17:48:52 +1300

    Style

--- resources/views/components/osu/activity.blade.php
+++ resources/views/components/osu/activity.blade.php
@@ -8,15 +8,23 @@
 
     <div class="flex flex-col gap-2">
         @foreach ($recent as $snipe)
-            <x-layout.card class="p-3 text-sm">
-                <p class="min-w-0 truncate">
-                    <span>{{ \Carbon\Carbon::parse($snipe['created_at'])->format('d/m/y') }}</span>
-                    <a href="/players/{{ $snipe['newUser']['id'] }}" class="font-bold hover:underline">{{ $snipe['newUser']['username'] }}</a>
-                    <span class="font-thin">sniped</span>
-                    <a href="/players/{{ $snipe['oldUser']['id'] }}" class="font-bold hover:underline">{{ $snipe['oldUser']['username'] }}</a>
-                    <span class="font-thin">on</span>
-                    <a href="/beatmaps/{{ $snipe['beatmap']['id'] }}" class="font-bold hover:underline">{{ $snipe['beatmap']['beatmapset']['title'] }} [{{ $snipe['beatmap']['version'] }}]</a>
-                </p>
+            <x-layout.card class="p-3 text-xs md:text-sm">
+                <div class="min-w-0">
+                    <div class="flex justify-between gap-2">
+                        <p class="truncate">
+                            <a href="/players/{{ $snipe['newUser']['id'] }}" class="font-bold hover:underline">{{ $snipe['newUser']['username'] }}</a>
+                            <span class="font-thin">sniped</span>
+                            <a href="/players/{{ $snipe['oldUser']['id'] }}" class="font-bold hover:underline">{{ $snipe['oldUser']['username'] }}</a>
+                        </p>
+
+                        <span class="">{{ \Carbon\Carbon::parse($snipe['created_at'])->format('d/m/y H:i') }}</span>
+                    </div>
+
+                    <div class="flex justify-between gap-2">
+                        <a href="/beatmaps/{{ $snipe['beatmap']['id'] }}" class="font-bold hover:underline truncate">{{ $snipe['beatmap']['beatmapset']['artist'] }} - {{ $snipe['beatmap']['beatmapset']['title'] }}</a>
+                        <a href="/beatmaps/{{ $snipe['beatmap']['id'] }}" class="font-bold hover:underline">[{{ $snipe['beatmap']['version'] }}]</a>
+                    </div>
+                </div>
             </x-layout.card>
         @endforeach
 

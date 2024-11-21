45c251c5750b49f206aeed9c64f291dd6b819884
James <54062686+nzbasic@users.noreply.github.com>
2024-11-21 20:45:43 +1300

    Test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,11 +41,11 @@
                             </x-form.button>
                         </div>
 
-                        <x-form.button
-                            onclick="Livewire.dispatch('openModal', { component: 'search-modal' })"
-                        >
-                            <x-lucide-search class="size-5"/>
-                        </x-form.button>
+{{--                        <x-form.button--}}
+{{--                            onclick="Livewire.dispatch('openModal', { component: 'search-modal' })"--}}
+{{--                        >--}}
+{{--                            <x-lucide-search class="size-5"/>--}}
+{{--                        </x-form.button>--}}
                     </div>
                 </div>
             </header>
